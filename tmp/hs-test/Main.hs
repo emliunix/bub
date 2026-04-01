6433e7ee77340909495f139ae2a677931457ba78
emliunix <emliunix@hotmail.com>
2026-04-01 12:33:18 +0800

    clean

--- tmp/hs-test/Main.hs
+++
@@ -1,7 +0,0 @@
-module Main (main) where
-
-main :: IO ()
-main = putStrLn "Hello, Haskell!"
-
-map :: Integer -> Integer
-map = id
--- tmp/hs-test/Main2.hs
+++
@@ -1,7 +0,0 @@
-module Main (main) where
-
-main :: IO ()
-main = print (map 42)
-
-map :: Integer -> Integer
-map = id
